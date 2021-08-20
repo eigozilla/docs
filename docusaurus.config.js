70a7ae1f0a40bb0b76a00b782bf33064a186bf01
HarukiKinoshita <harukikinoshita@outlook.com>
2021-08-20 23:20:24 +0900

    Fix wrong paths: editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/eigodzilla/eigodzilla/blob/master/',
           admonitions: {
             options: {
               infima: true,
@@ -144,7 +144,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/eigodzilla/eigodzilla/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
